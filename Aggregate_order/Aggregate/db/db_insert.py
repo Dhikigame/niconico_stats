d010152e5f1bf9a300afb0faf52448911c1f3375
daiki <dadada7772000@yahoo.co.jp>
2018-12-30 20:04:18 +0900

    動画IDがso拡張子の場合、コメントをDBに保存不可

--- Aggregate_order/Aggregate/db/db_insert.py
+++ Aggregate_order/Aggregate/db/db_insert.py
@@ -23,8 +23,10 @@ def db_regist(videoID):
 
     # 再生数取得
     video_view = video_info_get.view_video()
-    # コメント数取得
-    video_comment = video_info_get.comment_video()
+    # 動画IDがso拡張子以外の動画のコメント取得
+    if videoID[0:2] != "so":
+        # コメント数取得
+        video_comment = video_info_get.comment_video()
     # マイリスト数取得
     video_mylist = video_info_get.mylist_video()
 
@@ -41,40 +43,48 @@ def db_regist(videoID):
         exist = cursor.fetchone()
         # DBに同じ動画IDがなければ挿入する
         if exist is None:
-            sql = 'insert into video_data(video_id, title, view, comment, mylist, regist_date, upload_date'
-            for i in range(0, len(video_tags)):
-                sql += ', tag' + str(i+1) + ''
-            sql += ') values ("' + str(videoID) + '","' + str(video_title) + '",' + str(video_view) + ',' + str(video_comment) + ',' + str(video_mylist) + ',"' + str(datetime.today()) + '","' + str(video_date) + '"' 
-            for i in range(0, len(video_tags)):
-                video_tags[i] = video_tags[i].replace("'", "")
-                video_tags[i] = video_tags[i].replace('"', '')
-                sql += ', "' + str(video_tags[i]) + '"'
-            sql += ')' 
+            # 動画IDがso拡張子の場合はコメントをDBに登録しない
+            if videoID[0:2] != "so":
+                sql = 'insert into video_data(video_id, title, view, comment, mylist, regist_date, upload_date'
+                for i in range(0, len(video_tags)):
+                    sql += ', tag' + str(i+1) + ''
+                sql += ') values ("' + str(videoID) + '","' + str(video_title) + '",' + str(video_view) + ',' + str(video_comment) + ',' + str(video_mylist) + ',"' + str(datetime.today()) + '","' + str(video_date) + '"' 
+                for i in range(0, len(video_tags)):
+                    video_tags[i] = video_tags[i].replace("'", "")
+                    video_tags[i] = video_tags[i].replace('"', '')
+                    sql += ', "' + str(video_tags[i]) + '"'
+                sql += ')' 
+            else:
+                sql = 'insert into video_data(video_id, title, view, mylist, regist_date, upload_date'
+                for i in range(0, len(video_tags)):
+                    sql += ', tag' + str(i+1) + ''
+                sql += ') values ("' + str(videoID) + '","' + str(video_title) + '",' + str(video_view) + ',' + str(video_mylist) + ',"' + str(datetime.today()) + '","' + str(video_date) + '"' 
+                for i in range(0, len(video_tags)):
+                    video_tags[i] = video_tags[i].replace("'", "")
+                    video_tags[i] = video_tags[i].replace('"', '')
+                    sql += ', "' + str(video_tags[i]) + '"'
+                sql += ')' 
             print(sql)
             cursor.execute(sql)
             con.commit()
             print("insert:{0}".format(videoID))
         # DBに同じ動画IDがあれば最新の情報に更新する
         else:
-            sql = 'update video_data set title="' + str(video_title) + '", view=' + str(video_view) + ", comment=" + str(video_comment) + ", mylist=" + str(video_mylist) + ", regist_date='" + str(datetime.today()) + "', upload_date='" + str(video_date) + "'" #, tag1=?, tag2=?, tag3=?, tag4=?, tag5=?, tag6=?, tag7=?, tag8=?, tag9=?, tag10=?, tag11=? where video_id = ' + videoID + "'"
-            for i in range(0, len(video_tags)):
-                video_tags[i] = video_tags[i].replace("'", "")
-                video_tags[i] = video_tags[i].replace('"', '')
-                sql += ", tag" + str(i+1) + '= "' + str(video_tags[i]) + '"'
+            # 動画IDがso拡張子の場合はコメントをDBに登録しない
+            if videoID[0:2] != "so":           
+                sql = 'update video_data set title="' + str(video_title) + '", view=' + str(video_view) + ", comment=" + str(video_comment) + ", mylist=" + str(video_mylist) + ", regist_date='" + str(datetime.today()) + "', upload_date='" + str(video_date) + "'" #, tag1=?, tag2=?, tag3=?, tag4=?, tag5=?, tag6=?, tag7=?, tag8=?, tag9=?, tag10=?, tag11=? where video_id = ' + videoID + "'"
+                for i in range(0, len(video_tags)):
+                    video_tags[i] = video_tags[i].replace("'", "")
+                    video_tags[i] = video_tags[i].replace('"', '')
+                    sql += ", tag" + str(i+1) + '= "' + str(video_tags[i]) + '"'
+            else:           
+                sql = 'update video_data set title="' + str(video_title) + '", view=' + str(video_view) + ", mylist=" + str(video_mylist) + ", regist_date='" + str(datetime.today()) + "', upload_date='" + str(video_date) + "'" #, tag1=?, tag2=?, tag3=?, tag4=?, tag5=?, tag6=?, tag7=?, tag8=?, tag9=?, tag10=?, tag11=? where video_id = ' + videoID + "'"
+                for i in range(0, len(video_tags)):
+                    video_tags[i] = video_tags[i].replace("'", "")
+                    video_tags[i] = video_tags[i].replace('"', '')
+                    sql += ", tag" + str(i+1) + '= "' + str(video_tags[i]) + '"'
             sql += " where video_id = '" + videoID + "'"
             print(sql)
             cursor.execute(sql)
             con.commit()
             print("update:{0}".format(videoID))
-
-"""
-def position_videoid_regist(videoID):
-    db = sqlite3.connect('./db/videodata.db')
-
-    with closing(db) as con:
-        c = con.cursor()
-        cursor = db.cursor()
-        sql = "update videoID_position set position_videoid='" + str(videoID) + "'"
-        cursor.execute(sql)
-        con.commit()
-"""
